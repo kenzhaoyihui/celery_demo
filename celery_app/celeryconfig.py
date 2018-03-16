71d5e9e694a7f2c4a591e81b93a0e1cd04a05ace
yzhao <yzhao@redhat.com>
2018-03-16 18:26:39 +0800

    add the simple demo, no schedule

--- celery_app/celeryconfig.py
+++ celery_app/celeryconfig.py
@@ -1,26 +1,8 @@
-from datetime import timedelta
-from celery.schedules import crontab
-
 broker_url = 'redis://127.0.0.1:6379'
 result_backend = 'redis://127.0.0.1:6379/0'
 timezone = 'Asia/Shanghai'
 
 imports = (
     'celery_app.task1',
-    'celery_app.task2'
+     'celery_app.task2'
 )
-
-beat_schedule = {
-    'add-every-5-seconds':{
-        'task': 'celery_app.task1.add',
-        'schedule': timedelta(seconds=2),
-        'args': (5, 8)
-    },
-
-    'multiply-at-some-time':{
-        'task': 'celery_app.task2.multiply',
-        'schedule': crontab(minute='*/1'),
-        'args': (3, 7)
-    }
-}
-
---
+++ clients.py
@@ -0,0 +1,7 @@
+# from celery_app import task1, task2
+
+# result1 = task1.add.apply_async(args=[2, 8])
+# result2 = task2.multiply.apply_async(args=[2, 8])
+# print 'hello world'
+# print result1.get()
+# print result2.get()
